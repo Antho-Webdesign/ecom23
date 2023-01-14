d1c910014db69622872626e0512927575c73d7a2
Toutchy kipik <toutchy.kipik@outlook.com>
2023-01-14 23:56:13 +0100

    navbae + inc

--- store/views.py
+++ store/views.py
@@ -2,12 +2,16 @@ from django.shortcuts import render
 
 from store.models import Product
 
-
-# Create your views here.
-def home(request):
+# navabar
+def navbar(request):
     queryset = Product.objects.all()
     context = {
         'products': queryset
     }
-    return render(request, 'home.html', context)
+    return render(request, 'include/navbar.html', context)
+
+
+# home
+def home(request):
+    return render(request, 'home.html')
 
