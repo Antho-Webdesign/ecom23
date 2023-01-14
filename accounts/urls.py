9bba950aa8f58686fd85c27c48397e9e208bc54e
Toutchy kipik <toutchy.kipik@outlook.com>
2023-01-14 23:15:29 +0100

    navbae + inc

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from accounts.views import login_user, register, logout_user
 
 urlpatterns = [
-
+    path('login/', login_user, name='login'),
+    path('register/', register, name='register'),
+    path('logout/', logout_user, name='logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,40 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+
+from accounts.models import Customer
+
 
 # Create your views here.
-def login(request):
+
+def login_user(request):
+
+    if request.method == "POST":
+        # traiter le formulaire
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        if user := authenticate(request, username=username, password=password):
+            login(request, user)
+            return redirect('home')
     return render(request, 'accounts/login.html')
+
+def register(request):
+    if request.method == "POST":
+        # traiter le formulaire
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        password2 = request.POST.get("password2")
+        if password == password2:
+            user = Customer.objects.create_user(username, email, password)
+            profile = Customer.objects.create(user=user)
+            user.save()
+            profile.save()
+            return redirect('login')
+        else:
+            return redirect('register')
+    return render(request, 'accounts/register.html')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('home')
