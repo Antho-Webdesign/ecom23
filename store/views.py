c31bfd61c0bd313fce8ea5b1b51f31f34bc89ef7
AnthoWebDesign <toutchy.kipik@outlook.com>
2023-01-15 14:57:43 +0100

    dropdown navbar

--- store/views.py
+++ store/views.py
@@ -2,10 +2,11 @@ from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 from store.models import Product
 
+
 # navabar
 def navbar(request):
     user = request.user
-    products = Product.objects.all().order_by('-id')
+    product = Product.objects.all().order_by('-id')
 
     if name := request.GET.get('search'):
         if request.method == 'GET':
@@ -16,7 +17,7 @@ def navbar(request):
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     context = {
-        'products': products,
+        'products': product,
         'page_obj': page_obj,
         'user': user,
     }
