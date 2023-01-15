ff25490ecffea3640dad8725422829dcec86bc20
Toutchy kipik <toutchy.kipik@outlook.com>
2023-01-15 04:55:40 +0100

    model store

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from accounts.views import login_user, register, logout_user
+from accounts.views import login_user, register, logout_user, profile
 
 urlpatterns = [
     path('login/', login_user, name='login'),
     path('register/', register, name='register'),
     path('logout/', logout_user, name='logout'),
+    path('profile/', profile, name='profile'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth import authenticate, login, logout
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
-from accounts.models import Customer
+from accounts.models import Customer, Profile
+from store.models import Product
 
 
 # Create your views here.
@@ -38,3 +39,62 @@ def register(request):
 def logout_user(request):
     logout(request)
     return redirect('home')
+
+
+
+def profile(request):
+    user = request.user
+    profile = get_object_or_404(Profile, user=user)
+    products = Product.objects.all()
+    context = {
+        'products': products,
+        'profile': profile,
+        'user': user,
+    }
+    if request.method == "POST":
+        # traiter le formulaire
+        image = request.FILES.get("image")
+        profile.image = image
+        profile.save()
+        return redirect('profile')
+
+    return render(request, 'accounts/profile.html', context)
+
+
+def edit_profile(request):
+    pass
+    '''
+    user = request.user
+    profile = get_object_or_404(Profile, user=user)
+    context = {
+        'profile': profile,
+    }
+    if request.method == "POST":
+        # traiter le formulaire
+        username = request.POST.get("username")
+        email = request.POST.get("email")
+        phone = request.POST.get("phone")
+        address = request.POST.get("address")
+        city = request.POST.get("city")
+        zip_code = request.POST.get("zip_code")
+        state = request.POST.get("state")
+        country = request.POST.get("country")
+        user.username = username
+        user.email = email
+        profile.phone = phone
+        profile.address = address
+        profile.city = city
+        profile.zip_code = zip_code
+        profile.state = state
+        profile.country = country
+        message = "Your profile has been updated successfully"
+        msg = {
+            'message': message,
+        }
+        context.update(msg)
+
+        user.save()
+        profile.save()
+        return redirect('profile')
+        '''
+    return render(request, 'accounts/profile_update.html')
