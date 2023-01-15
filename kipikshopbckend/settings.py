3a65dfd91b27bd6e9df243527bdcd5547163a700
Toutchy kipik <toutchy.kipik@outlook.com>
2023-01-15 05:16:19 +0100

    model store

--- kipikshopbckend/settings.py
+++ kipikshopbckend/settings.py
@@ -125,7 +125,12 @@ LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'login'
 
 # Email settings
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = ' '
+# EMAIL_HOST_PASSWORD = ' '
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
